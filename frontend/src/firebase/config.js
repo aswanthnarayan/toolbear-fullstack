72ada935dfcc2ced6a30505f1b50f69134f3fc80
aswanthnarayan <aswanthnarayanan@gmail.com>
2024-11-21 01:42:40 +0530

    completed Auth 90% google sigin-bug fix needed , basic component and routing completed

--- frontend/src/firebase/config.js
+++ frontend/src/firebase/config.js
@@ -6,15 +6,16 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCGcFgCmpLmkJJgW65RBz2fX2S-EpSsgiw",
-    authDomain: "toolbear-58c74.firebaseapp.com",
-    projectId: "toolbear-58c74",
-    storageBucket: "toolbear-58c74.firebasestorage.app",
-    messagingSenderId: "245871246870",
-    appId: "1:245871246870:web:c300ddc542474da2a3ff37",
-    measurementId: "G-RTS5XM5P67"
-  };
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
