276d640842a636c2a45595a3a690fa3c9f5842e1
aswanthnarayan <aswanthnarayanan@gmail.com>
2025-04-05 16:27:34 +0530

    updates for render deployment

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,18 @@ import {connectDb} from './config/database.js'
 import authRoutes from './routes/authRoutes.js'
 import adminRoutes from './routes/adminRoutes.js'
 import userRoutes from './routes/userRoutes.js'
+import cors from 'cors';
+
 
 const port = process.env.PORT || 5000
 const app = express();
 
 connectDb()
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    credentials: true, // allow cookies
+  }));
 
 app.use(json())
 app.use(express.json());
